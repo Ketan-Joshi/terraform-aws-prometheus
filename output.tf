02abb1a3f038d8c2ff3361003400df04be18fb5f
Ketan Joshi <ketanjoshi@TTNs-MacBook-Pro.local>
2023-03-06 16:11:22 +0530

    Fixes

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "private_ip" {
   value = aws_instance.prometheus.private_ip
 }
-output "pem_file" {
+output "monitoring_pem_file" {
   value = tls_private_key.ssh_private_key.private_key_pem
 }
